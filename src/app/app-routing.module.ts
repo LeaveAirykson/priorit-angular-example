5c8757d8d7b5b512f662e2b9847ee1c6f039d973
LeaveAirykson <georg.boete@gmail.com>
2024-06-24 12:40:27 +0200

    Add bookform booklist and navigation comp

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BooklistComponent } from './booklist/booklist.component';
+import { BookformComponent } from './bookform/bookform.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: BooklistComponent
+  },
+  {
+    path: 'add',
+    component: BookformComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BooklistComponent } from './booklist/booklist.component';
+import { BookformComponent } from './bookform/bookform.component';
+import { NavigationComponent } from './navigation/navigation.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BooklistComponent,
+    BookformComponent,
+    NavigationComponent
   ],
   imports: [
     BrowserModule,
