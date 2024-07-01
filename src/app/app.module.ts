adf65fb44f15ca2cc26312c51c81ac5878e4dbbb
LeaveAirykson <georg.boete@gmail.com>
2024-07-01 17:16:48 +0200

    Remove navigation component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BooklistComponent } from './booklist/booklist.component';
 import { BookformComponent } from './bookform/bookform.component';
-import { NavigationComponent } from './navigation/navigation.component';
+import { BooklistComponent } from './booklist/booklist.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     BooklistComponent,
     BookformComponent,
-    NavigationComponent
   ],
   imports: [
     BrowserModule,
