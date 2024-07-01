a5b56330faa2a6d3e9842299843ba7fa3652f52c
LeaveAirykson <georg.boete@gmail.com>
2024-07-01 17:21:47 +0200

    Implement booklist component logic

--- src/app/booklist/booklist.component.ts
+++ src/app/booklist/booklist.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Router } from '@angular/router';
+import { Book } from 'src/app/interfaces/book.interface';
+import { StorageService } from 'src/app/services/storage.service';
 
 @Component({
   selector: 'app-booklist',
@@ -6,5 +9,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./booklist.component.css']
 })
 export class BooklistComponent {
+  @Input() data: Book[] = [];
+  @Output() update: EventEmitter<string> = new EventEmitter();
 
+  constructor(private router: Router, private storage: StorageService) { }
+
+  edit(id: string) {
+    this.router.navigate(['/edit', id]);
+  }
+
+  remove(id: string) {
+    this.storage.removeById(id)
+      .subscribe((response) => {
+        if (response.success) {
+          console.log(response.message);
+          this.update.emit(id);
+        }
+      })
+  }
 }
