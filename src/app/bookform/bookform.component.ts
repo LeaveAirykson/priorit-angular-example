48399eff548e287d615ef6c83f5da72135de44c2
LeaveAirykson <georg.boete@gmail.com>
2024-07-01 17:22:09 +0200

    Implement bookform component logic

--- src/app/bookform/bookform.component.ts
+++ src/app/bookform/bookform.component.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { StorageService } from 'src/app/services/storage.service';
+import { isbnChecksumValidator, isbnFormatValidator, isbnUsedAsyncValidator } from '../utilities/book.validator';
+import { patterns } from './validationpatterns';
 
 @Component({
   selector: 'app-bookform',
@@ -6,5 +11,74 @@ import { Component } from '@angular/core';
   styleUrls: ['./bookform.component.css']
 })
 export class BookformComponent {
+  form!: FormGroup;
+  editmode = false;
+  patterns = patterns;
 
+  @Input() set id(id: string) {
+    if (!id) {
+      this.editmode = false;
+      return;
+    }
+
+    this.storage.getById(id)
+      .subscribe((val) => {
+        if (!val) {
+          this.editmode = false;
+          this.router.navigate(['/add']);
+          return;
+        }
+
+        this.editmode = true;
+        this.form.patchValue(val);
+      });
+  }
+
+  control(id: string) {
+    return this.form.controls[id] as FormControl;
+  }
+
+  constructor(
+    private fb: FormBuilder,
+    private storage: StorageService,
+    private router: Router) {
+
+    this.form = this.fb.group({
+      id: [null],
+      title: ['', [Validators.required]],
+      isbn: ['',
+        [Validators.required, isbnFormatValidator(), isbnChecksumValidator()],
+        [isbnUsedAsyncValidator(this.storage)]
+      ],
+      year: ['', [Validators.pattern(patterns.year)]],
+      pagecount: ['', [Validators.pattern(patterns.pagecount)]],
+      language: ['Deutsch', { nonNullable: true }],
+      group: ['', [Validators.pattern(patterns.ddc)]],
+    });
+  }
+
+  showErrors(id: string) {
+    const control = this.form.get(id) as FormControl;
+    return control.invalid && control.dirty && control.errors;
+  }
+
+  submit() {
+    if (!this.form.valid) {
+      return;
+    }
+
+    if (this.editmode) {
+      this.storage.update(this.form.value['id'], this.form.value)
+        .subscribe((response) => {
+          console.log(response.message);
+          this.router.navigate(['/add']);
+        });
+    } else {
+      this.storage.create(this.form.value)
+        .subscribe((response) => {
+          this.form.reset();
+          console.log(response.message);
+        })
+    }
+  }
 }
