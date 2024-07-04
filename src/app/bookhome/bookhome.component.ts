1e97d3a77da266d1c48bd8d2500d8fd79e165019
LeaveAirykson <georg.boete@gmail.com>
2024-07-04 12:08:57 +0200

    Add docs to bookhome

--- src/app/bookhome/bookhome.component.ts
+++ src/app/bookhome/bookhome.component.ts
@@ -1,6 +1,10 @@
 import { Component } from '@angular/core';
 import { ActivationEnd, Router } from '@angular/router';
 
+/**
+ * Component used as wrapper around booklist view.
+ * It handles showing form and edit state based on query params.
+ */
 @Component({
   selector: 'app-bookhome',
   templateUrl: './bookhome.component.html',
@@ -8,7 +12,13 @@ import { ActivationEnd, Router } from '@angular/router';
 export class BookhomeComponent {
   id!: string;
   formVisible = false;
-  validateIsbnChecksum = true;
+  /**
+   * unspecific options used to change behaviour
+   * of the example application
+   */
+  options: { [key: string]: any; } = {
+    validateIsbnChecksum: true
+  }
 
   constructor(private router: Router) {
     this.router.events.subscribe((e) => {
@@ -19,10 +29,18 @@ export class BookhomeComponent {
     });
   }
 
-  exampleOptions(opt: { key: string; val: any }) {
-    if (opt.key == 'validateIsbnChecksum') {
-      this.validateIsbnChecksum = opt.val;
-    }
+  /**
+   * Overrides example options to change
+   * behaviour of the example application
+   *
+   * @param  {object} opt
+   * @param  {string} opt.key
+   * @param  {any}    opt.val
+   *
+   * @return {void}
+   */
+  overrideOption(opt: { key: string; val: any }) {
+    this.options[opt.key] = opt.val;
   }
 
   hideForm() {
