044b8a82291328f52cd4c65ffc0de780db1c69e1
LeaveAirykson <georg.boete@gmail.com>
2024-07-05 12:12:45 +0200

    Add bookfilter interface

---
+++ src/app/interfaces/bookfilter.interface.ts
@@ -0,0 +1,13 @@
+export type BookFilter = {
+  year: string;
+  yearStart: number;
+  yearEnd: number;
+  pagecount: string;
+  pagecountStart: number;
+  pagecountEnd: number;
+  language: string[];
+  ddc: string;
+  remuneration: string;
+  remunerationStart: number;
+  remunerationEnd: string;
+}
