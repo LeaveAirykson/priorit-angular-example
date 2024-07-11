c77eb1cdcd181f3466c26ad298cbf7fa4cb8d25a
LeaveAirykson <georg.boete@gmail.com>
2024-07-11 20:31:54 +0200

    Add unit test for remuneration pipe

--- src/app/book/pipes/remuneration.pipe.spec.ts
+++ src/app/book/pipes/remuneration.pipe.spec.ts
@@ -1,8 +1,31 @@
+import { Book } from '../models/book.class';
 import { RemunerationPipe } from './remuneration.pipe';
 
 describe('RemunerationPipe', () => {
   it('create an instance', () => {
     const pipe = new RemunerationPipe();
     expect(pipe).toBeTruthy();
+  });
+
+  it('calculate remuneration', () => {
+    const pipe = new RemunerationPipe();
+    const book = new Book({
+      language: 'Deutsch',
+      year: 1990,
+      title: 'A',
+      isbn: ' 3-86680-192-0',
+      pagecount: 100
+    });
+    const book2 = new Book({
+      language: 'Englisch',
+      year: 1989,
+      title: 'A',
+      isbn: ' 3-86680-192-0',
+      pagecount: 100
+    });
+
+    expect(pipe.transform(book)).toBe(121);
+    expect(pipe.transform(book2)).toBe(125);
+
   });
 });
